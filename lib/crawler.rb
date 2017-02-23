e1f51ccefe741e98a81f727e6bd2efcd569d9356
grekuna <gregor.martius@gmail.com>
2017-02-23 09:50:41 +0100

    Working import and display

--- lib/crawler.rb
+++ lib/crawler.rb
@@ -32,11 +32,13 @@ module Crawler
     #TODO: Loop over multiple sites
     #get aphorisms for author
     def get_aphorisms
-      binding.pry
+      #binding.pry
       Author.all.each do |author|
         site_author = get_html("http://www.aphorism4all.com/" + author.url)
         site_author.css('#content strong').each do |node|
-          author.aphorisms.create(aphorism: node.inner_text)
+          aphorism = node.inner_text.strip
+          unless aphorism  =~ /^Pages/
+            author.aphorisms.find_or_create_by(aphorism: aphorism)
         end
       end
     end
