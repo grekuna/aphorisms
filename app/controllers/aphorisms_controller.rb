ca8ca09c371fed731e4cf59cc5e77c9040e8f93c
grekuna <gregor.martius@gmail.com>
2017-02-23 15:30:23 +0100

    switch to postgres

--- app/controllers/aphorisms_controller.rb
+++ app/controllers/aphorisms_controller.rb
@@ -2,8 +2,6 @@ class AphorismsController < ApplicationController
 
   def index
      @author = Author.find(rand(0..Author.all.count-1))
-     @aphorism = @author.aphorisms.first.aphorism
-
-     #@author.aphorisms.find(rand(0..@author.aphorisms.count-1))
+     @aphorism = @author.aphorisms.find(rand(@author.aphorisms.first.id..@author.aphorisms.last.id)).aphorism
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170222134918) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "aphorisms", force: :cascade do |t|
     t.integer  "author_id"
     t.text     "aphorism"
