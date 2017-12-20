abc88c110e3745aed9c89736a32b8a2e1587e6cd
grekuna <gregor.martius@gmail.com>
2017-12-20 11:31:01 +0100

    fix crawler bug

--- app/controllers/aphorisms_controller.rb
+++ app/controllers/aphorisms_controller.rb
@@ -12,7 +12,7 @@ class AphorismsController < ApplicationController
 
     origin_body = Curl.get(origin_url).body_str
     origin = Nokogiri::HTML(origin_body)
-    @image_url = origin.css('#content-image').attr('data-src').value
+    @image_url = origin.css('.image-page-style-image--1XCzC').attr('src').value
 
     @author = Author.find(rand(0..Author.all.count-1))
     @aphorism = @author.aphorisms.find(rand(@author.aphorisms.first.id..@author.aphorisms.last.id)).aphorism
