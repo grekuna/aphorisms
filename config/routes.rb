743220156425c1d4a438f58be175921dce84891f
grekuna <gregor.martius@gmail.com>
2018-01-04 10:05:51 +0100

    change root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root to: 'aphorisms#index'
+root to: 'aphorisms#start'
 
 get 'aphorisms', to: 'aphorisms#index'
 
