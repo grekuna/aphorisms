33fdda2a9e5962d97417ed3433e539c2b117e052
grekuna <gregor.martius@gmail.com>
2018-09-10 20:56:59 +0200

    static url

--- app/controllers/aphorisms_controller.rb
+++ app/controllers/aphorisms_controller.rb
@@ -13,13 +13,10 @@ class AphorismsController < ApplicationController
     # origin_body = Curl.get(origin_url).body_str
     # origin = Nokogiri::HTML(origin_body)
     # @image_url = origin.css('._1XCzC').attr('src').value
-    unless (html_body = Curl.get("http://calvinandhobbes-daily.tumblr.com/").body_str == nil)
-      html_body = Curl.get("http://calvinandhobbes-daily.tumblr.com/").body_str
-      site = Nokogiri::HTML(html_body)
-      @image_url= site.css('img').first.attr('src')
-    else 
-      @image_url= "https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Solid_white.svg/2000px-Solid_white.svg.png"
-    end
+      # html_body = Curl.get("http://calvinandhobbes-daily.tumblr.com/").body_str
+      # site = Nokogiri::HTML(html_body)
+      # @image_url= site.css('img').first.attr('src')
+    @image_url= "https://78.media.tumblr.com/4181c5ebb3bd17fb2379cfa75617805b/tumblr_mf5l1mXBnI1r4ejnio1_640.gif"
  
 
     @author = Author.find(rand(0..Author.all.count-1))
