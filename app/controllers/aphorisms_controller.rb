30f7cca83a5ce863932a7e8babd078108dc00f2c
grekuna <gregor.martius@gmail.com>
2018-09-10 20:49:57 +0200

    primitve error handling calvin

--- app/controllers/aphorisms_controller.rb
+++ app/controllers/aphorisms_controller.rb
@@ -13,10 +13,13 @@ class AphorismsController < ApplicationController
     # origin_body = Curl.get(origin_url).body_str
     # origin = Nokogiri::HTML(origin_body)
     # @image_url = origin.css('._1XCzC').attr('src').value
-
-    html_body = Curl.get("http://calvinandhobbes-daily.tumblr.com/").body_str
-    site = Nokogiri::HTML(html_body)
-    @image_url= site.css('img').first.attr('src')
+    unless (html_body = Curl.get("http://calvinandhobbes-daily.tumblr.com/").body_str == nil)
+      html_body = Curl.get("http://calvinandhobbes-daily.tumblr.com/").body_str
+      site = Nokogiri::HTML(html_body)
+      @image_url= site.css('img').first.attr('src')
+    else 
+      @image_url= "https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Solid_white.svg/2000px-Solid_white.svg.png"
+    end
  
 
     @author = Author.find(rand(0..Author.all.count-1))
