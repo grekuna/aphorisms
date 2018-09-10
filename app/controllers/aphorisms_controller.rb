d0fe8a2545b1730a2d555325d46ef7bc9e0aa8f4
grekuna <gregor.martius@gmail.com>
2018-09-10 21:36:29 +0200

    static calvin url as quick bug fix

--- app/controllers/aphorisms_controller.rb
+++ app/controllers/aphorisms_controller.rb
@@ -13,12 +13,14 @@ class AphorismsController < ApplicationController
     # origin_body = Curl.get(origin_url).body_str
     # origin = Nokogiri::HTML(origin_body)
     # @image_url = origin.css('._1XCzC').attr('src').value
-      # html_body = Curl.get("http://calvinandhobbes-daily.tumblr.com/").body_str
-      # site = Nokogiri::HTML(html_body)
-      # @image_url= site.css('img').first.attr('src')
-    @image_url= "https://78.media.tumblr.com/4181c5ebb3bd17fb2379cfa75617805b/tumblr_mf5l1mXBnI1r4ejnio1_640.gif"
- 
 
+    #Bug scraping works locally but not on heroku
+
+    # html_body = Curl.get("http://calvinandhobbes-daily.tumblr.com/").body_str
+    # site = Nokogiri::HTML(html_body)
+    # @image_url= site.css('img').first.attr('src')
+    
+    @image_url= "https://78.media.tumblr.com/4181c5ebb3bd17fb2379cfa75617805b/tumblr_mf5l1mXBnI1r4ejnio1_640.gif"
     @author = Author.find(rand(0..Author.all.count-1))
     @aphorism = @author.aphorisms.find(rand(@author.aphorisms.first.id..@author.aphorisms.last.id)).aphorism
   end
